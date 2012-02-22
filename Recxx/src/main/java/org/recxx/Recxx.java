ad1642d84b8ed972c2f166eed025f8139df98af2
Mike Day <a.michael.day@gmail.com>
2012-02-22 21:28:47 +0000

    Correct total row counts issue

--- Recxx/src/main/java/org/recxx/Recxx.java
+++ Recxx/src/main/java/org/recxx/Recxx.java
@@ -284,6 +284,7 @@ public class Recxx extends AbstractRecFeed implements Runnable {
 								        inputColumns1[input1CompareColumnPosition[i]], o1, input2Alias,
 								        inputColumns2[input2CompareColumnPosition[i]], o2, valueOf(result.getPercentageDifference()),
 								        valueOf(result.getAbsoluteDifference()));
+								matchedRow = false;
 							}
 						
 						}
@@ -434,6 +435,7 @@ public class Recxx extends AbstractRecFeed implements Runnable {
 								        inputColumns1[input1CompareColumnPosition[i]], o1, input2Alias,
 								        inputColumns2[input2CompareColumnPosition[i]], o2, valueOf(result.getPercentageDifference()),
 								        valueOf(result.getAbsoluteDifference()));
+								matchedRow = false;
 							}
 						
 						}
@@ -774,7 +776,7 @@ public class Recxx extends AbstractRecFeed implements Runnable {
 	 */
 	private void logDifferenceToFile(String key, String columnName1, Object columnValue1, String columnName2,
 	        Object columnValue2, String percentageDiff, String absDiff) throws IOException {
-		StringTokenizer st = new StringTokenizer(key, "¶");
+		StringTokenizer st = new StringTokenizer(key, "ï¿½");
 
 		while (st.hasMoreTokens()) {
 			m_logger.write(st.nextToken());
